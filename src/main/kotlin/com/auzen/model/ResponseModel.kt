cc86e2a5b9fd8b32d8a44dd6dc279d4f72f3fd44
Egit <egit.saputra@bareksa.com>
2020-04-08 06:01:05 +0700

    add status, message, data to ResponseModel.kt

--- src/main/kotlin/com/auzen/model/ResponseModel.kt
+++ src/main/kotlin/com/auzen/model/ResponseModel.kt
@@ -3,5 +3,10 @@ package com.auzen.model
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 
-class ResponseModel<T>(status: HttpStatus = HttpStatus.OK) : ResponseEntity<T>(status) {
+open class ResponseModel(val status: Boolean = false,
+                         val message: String? = null,
+                         val data: Any?,
+                         httpCode: HttpStatus = HttpStatus.OK)
+    : ResponseEntity<ResponseModel>(httpCode) {
+
 }
