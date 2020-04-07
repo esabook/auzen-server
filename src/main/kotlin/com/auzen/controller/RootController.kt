710d6c97354cb21ab79bc23ae225bb141ddc298f
Egit <egit.saputra@bareksa.com>
2020-04-08 05:38:36 +0700

    redirect to /swagger-ui.html from /help

--- src/main/kotlin/com/auzen/controller/RootController.kt
+++ src/main/kotlin/com/auzen/controller/RootController.kt
@@ -1,21 +1,14 @@
 package com.auzen.controller
 
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 @RestController
 class RootController {
 
-    @GetMapping("/help")
-    fun getRoot() = "help"
-
-    @GetMapping("/throw")
-    fun getThrow() {
-        throw Exception("from getThrow()")
-    }
-
-    @GetMapping("/error")
-    fun getError(req: HttpServletRequest) = "silent"
+    @RequestMapping("/help")
+    fun getRoot(req: HttpServletRequest, res: HttpServletResponse) = res.sendRedirect("/swagger-ui.html")
 
 }
