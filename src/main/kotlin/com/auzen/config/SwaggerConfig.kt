5b94d08a00b282cdbd8babee562da1064e60baad
Egit <egit.saputra@bareksa.com>
2020-04-08 04:43:02 +0700

    add authorization helper/shortcut to swaggerDoc

--- src/main/kotlin/com/auzen/config/SwaggerConfig.kt
+++ src/main/kotlin/com/auzen/config/SwaggerConfig.kt
@@ -1,15 +1,23 @@
 package com.auzen.config
 
+import com.auzen.security.SecurityConstants
+import io.swagger.models.auth.In
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpHeaders
 import springfox.documentation.builders.ApiInfoBuilder
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
 import springfox.documentation.service.ApiInfo
+import springfox.documentation.service.ApiKey
 import springfox.documentation.service.Contact
+import springfox.documentation.service.SecurityReference
 import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spi.service.contexts.SecurityContext
 import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger.web.*
 import springfox.documentation.swagger2.annotations.EnableSwagger2
+
 
 @Configuration
 @EnableSwagger2
@@ -21,6 +29,9 @@ class SwaggerConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
+                .useDefaultResponseMessages(true)
+                .securitySchemes(arrayListOf(apiKey()))
+                .securityContexts(listOf(securityContext()))
                 .apiInfo(apiEndPointsInfo())
     }
 
@@ -30,6 +41,41 @@ class SwaggerConfig {
                 .description("Auzen Management REST API")
                 .contact(Contact("Egit", "@egitsptr", null))
                 .version("1.0.0")
+                .build()
+    }
+
+    private fun apiKey(): ApiKey? {
+        return ApiKey(SecurityConstants.TOKEN_PREFIX, HttpHeaders.AUTHORIZATION, In.HEADER.name)
+    }
+
+    private fun securityContext(): SecurityContext? {
+        val securityReference = SecurityReference.builder()
+                .reference(SecurityConstants.TOKEN_PREFIX)
+                .scopes(arrayOf())
+                .build()
+        return SecurityContext
+                .builder()
+                .securityReferences(listOf(securityReference))
+                .build()
+    }
+
+    @Bean
+    fun uiConfig(): UiConfiguration? {
+        return UiConfigurationBuilder.builder()
+                .deepLinking(true)
+                .displayOperationId(false)
+                .defaultModelsExpandDepth(1)
+                .defaultModelExpandDepth(1)
+                .defaultModelRendering(ModelRendering.EXAMPLE)
+                .displayRequestDuration(false)
+                .docExpansion(DocExpansion.NONE)
+                .filter(false)
+                .maxDisplayedTags(null)
+                .operationsSorter(OperationsSorter.METHOD)
+                .showExtensions(false)
+                .tagsSorter(TagsSorter.ALPHA)
+                .supportedSubmitMethods(UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS)
+                .validatorUrl(null)
                 .build()
     }
 }
