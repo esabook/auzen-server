2e265b2d0a9e377d03999efa5854f960c50e83eb
Egit <egit.saputra@bareksa.com>
2020-04-08 23:51:17 +0700

    exclude in-built Spring controller from swaggerDoc

--- src/main/kotlin/com/auzen/config/SwaggerConfig.kt
+++ src/main/kotlin/com/auzen/config/SwaggerConfig.kt
@@ -5,6 +5,7 @@ import io.swagger.models.auth.In
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpHeaders
+import org.springframework.web.bind.annotation.RestController
 import springfox.documentation.builders.ApiInfoBuilder
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
@@ -26,7 +27,7 @@ class SwaggerConfig {
     fun api(): Docket {
         return Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController::class.java))
                 .paths(PathSelectors.any())
                 .build()
                 .useDefaultResponseMessages(true)
